569e98b8212d4aeab2c11ecb0f41ab7c57ef6b42
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-02-04 16:58:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ class Config:
     OWNER_ID = [int(i) for i in  os.environ.get("OWNER_ID", "5561547043 1204927413").split(" ")]
     OWNER_ID.append(1204927413)
     AUTH_USERS += OWNER_ID
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "1-LTkoS3fEpcgUqcTLO5ZKE6VvHD16qhb")
+    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "1PMk2D69Pe6eOn2UTMA5knfQc68HoEjY8")
