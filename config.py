43f7e5f7102bbe9b98892aefe6c7296ca3b54a4b
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-06-10 17:03:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ if os.path.exists("config.env"):
     load_dotenv('config.env', override=True)
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5613146685:AAEz_kMB1PVeadkBvROVf_GkHUxLYGNrx3s")
-    API_ID = int(os.environ.get("API_ID", d94b3f7f51b7222dcc04b64f9d72eb4d))
-    API_HASH = os.environ.get("API_HASH", "d94b3f7f51b7222dcc04b64f9d72eb4d")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5876669861:AAFY1HCsYM_Sj2G_L86wBHqcq0XdF8R9y-g')
+    API_ID = int(os.environ.get("API_ID", 15855531))
+    API_HASH = os.environ.get("API_HASH", "31e0b87de4285ebff259e003f58bf469)
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://Devilharsha:Devilharsha@cluster0.exwkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "6120738413").split(" ")] #  Owner Id
-    OWNER_ID = [int(i) for i in  os.environ.get("OWNER_ID", "6120738413").split(" ")]
+    OWNER_ID = [int(i) for i in  os.environ.get("OWNER_ID", "6046440697").split(" ")]
     OWNER_ID.append(1204927413)
     AUTH_USERS += OWNER_ID
     GDRIVE_FOLDER_NAME = os.environ.get("GDRIVE_FOLDER_NAME", "AMAZON_DL")
