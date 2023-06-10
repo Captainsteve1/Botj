b3ae5f5ea46b9a4d2740fb948a85c6c9bf1e137e
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-06-10 17:25:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ if os.path.exists("config.env"):
     load_dotenv('config.env', override=True)
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5876669861:AAFY1HCsYM_Sj2G_L86wBHqcq0XdF8R9y-g')
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5876669861:AAFY1HCsYM_Sj2G_L86wBHqcq0XdF8R9y-g")
     API_ID = int(os.environ.get("API_ID", 15855531))
-    API_HASH = os.environ.get("API_HASH", "31e0b87de4285ebff259e003f58bf469)
+    API_HASH = os.environ.get("API_HASH", "31e0b87de4285ebff259e003f58bf469")
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://Devilharsha:Devilharsha@cluster0.exwkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "6120738413").split(" ")] #  Owner Id
     OWNER_ID = [int(i) for i in  os.environ.get("OWNER_ID", "6046440697").split(" ")]
