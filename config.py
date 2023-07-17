e19e1c0ab66882b55b5fc4cb5fdca561dec1b8d9
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-07-17 16:46:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6308793161:AAF90Gr6Umq0Fng4BBUKVyIA-rE1Po1HTPk")
     API_ID = int(os.environ.get("API_ID", 15855531))
     API_HASH = os.environ.get("API_HASH", "31e0b87de4285ebff259e003f58bf469")
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://Devilharsha:Devilharsha@cluster0.exwkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://jaajsjsksjwbwbwpq:blN8PY4Z2LLtBHHZ@cluster0.ffrx2h5.mongodb.net/?retryWrites=true&w=majority")
     AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "6046440697").split(" ")] #  Owner Id
     OWNER_ID = [int(i) for i in  os.environ.get("OWNER_ID", "6046440697").split(" ")]
     OWNER_ID.append(1204927413)
